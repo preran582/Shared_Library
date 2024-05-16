5bd202542c9804fbb1a2bd3f723e026d38b002ef
root <root@Ubuntu.myguest.virtualbox.org>
2024-05-16 22:52:03 +0530

    commit

--- vars/runDockerContainer.groovy
+++ vars/runDockerContainer.groovy
@@ -3,7 +3,7 @@
 def call() {
     stage('Run Docker container') {
         steps {
-            sh "docker run --name new_App -d -p 8080:8080 prerana004/my-app:2.0.0"
+            sh "docker run --name new_App_groovy -d -p 8082:8080 prerana004/my-app:2.0.0"
         }
     }
 }
